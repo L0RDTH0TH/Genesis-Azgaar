b4847daa4c09cf9e078ef7724b750931ff85f134
LORDTHOTH <forlordthoth@gmail.com>
2026-01-03 20:16:12 -0500

    feat/azgaar-fork: Fix 1.2 - Restore original continents template sequence (remove extra smoothing, restore 3-4 trough/pit counts)

--- src/core/heightmap.js
+++ src/core/heightmap.js
@@ -336,19 +336,15 @@ function generateContinentTemplate(grid, options, rng) {
   const template = new HeightmapTemplate(grid, options, rng);
   template.setHeights(heights);
 
-  // Execute continent template steps (from original config/heightmap-templates.js)
-  // Modified for better cohesion: larger initial blobs, more aggressive smoothing
+  // Execute continent template steps (exact match from original/config/heightmap-templates.js lines 41-55)
   // Hill 1 80-85 60-80 40-60
   template.addHill('1', '80-85', '60-80', '40-60');
   
   // Hill 1 80-85 20-30 40-60
   template.addHill('1', '80-85', '20-30', '40-60');
   
-  // Hill 6-7 15-30 25-75 15-85 (larger hills for cohesion)
+  // Hill 6-7 15-30 25-75 15-85
   template.addHill('6-7', '15-30', '25-75', '15-85');
-  
-  // First aggressive smooth to merge initial blobs
-  template.smooth(4, 0);
   
   // Multiply 0.6 land 0 0
   template.modify('land', 0, 0.6);
@@ -365,9 +361,6 @@ function generateContinentTemplate(grid, options, rng) {
   // Range 0-3 30-60 80-90 20-80
   template.addRange('0-3', '30-60', '80-90', '20-80');
   
-  // Second aggressive smooth before straits
-  template.smooth(3, 0);
-  
   // Strait 2 vertical 0 0
   template.addStrait('2', 'vertical');
   
@@ -377,25 +370,17 @@ function generateContinentTemplate(grid, options, rng) {
   // Smooth 3 0 0 0
   template.smooth(3, 0);
   
-  // Reduced trough/pit counts for less fragmentation
-  // Trough 2-3 15-20 15-85 20-80 (reduced from 3-4)
-  template.addTrough('2-3', '15-20', '15-85', '20-80');
+  // Trough 3-4 15-20 15-85 20-80
+  template.addTrough('3-4', '15-20', '15-85', '20-80');
   
-  // Trough 2-3 5-10 45-55 45-55 (reduced from 3-4)
-  template.addTrough('2-3', '5-10', '45-55', '45-55');
+  // Trough 3-4 5-10 45-55 45-55
+  template.addTrough('3-4', '5-10', '45-55', '45-55');
   
-  // Pit 2-3 10-20 15-85 20-80 (reduced from 3-4)
-  template.addPit('2-3', '10-20', '15-85', '20-80');
-  
-  // Multiple aggressive smooth passes to reduce fragmentation
-  template.smooth(4, 0);
-  template.smooth(3, 0);
+  // Pit 3-4 10-20 15-85 20-80
+  template.addPit('3-4', '10-20', '15-85', '20-80');
   
   // Mask 4 0 0 0
   template.mask(4);
-  
-  // Final smooth pass after masking
-  template.smooth(2, 0);
 
   return template.getHeights();
 }
